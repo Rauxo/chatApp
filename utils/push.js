6da6ffe9d44035218f3efa6d90fcc72adf0df8b4
Rauxo <rahulsaikia682@gmail.com>
2026-03-31 16:34:00 +0530

    recode push notification

--- utils/push.js
+++ utils/push.js
@@ -1,11 +1,50 @@
-const { Expo } = require('expo-server-sdk');
+// const { Expo } = require('expo-server-sdk');
 
-let expo = new Expo();
+// let expo = new Expo();
+
+// const sendPushNotification = async (somePushTokens, messageBody, data = {}) => {
+//     let messages = [];
+//     for (let pushToken of somePushTokens) {
+//         if (!Expo.isExpoPushToken(pushToken)) {
+//             console.error(`Push token ${pushToken} is not a valid Expo push token`);
+//             continue;
+//         }
+
+//         messages.push({
+//             to: pushToken,
+//             sound: 'default',
+//             body: messageBody,
+//             data: data,
+//         });
+//     }
+
+//     let chunks = expo.chunkPushNotifications(messages);
+//     let tickets = [];
+    
+//     for (let chunk of chunks) {
+//         try {
+//             let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+//             tickets.push(...ticketChunk);
+//         } catch (error) {
+//             console.error('Error sending push notification chunk', error);
+//         }
+//     }
+// };
+
+// module.exports = { sendPushNotification };
+let expo;
 
 const sendPushNotification = async (somePushTokens, messageBody, data = {}) => {
+    // 🔥 Dynamic import (fix for ESM)
+    if (!expo) {
+        const { Expo } = await import('expo-server-sdk');
+        expo = new Expo();
+    }
+
     let messages = [];
+
     for (let pushToken of somePushTokens) {
-        if (!Expo.isExpoPushToken(pushToken)) {
+        if (!expo.constructor.isExpoPushToken(pushToken)) {
             console.error(`Push token ${pushToken} is not a valid Expo push token`);
             continue;
         }
@@ -20,7 +59,7 @@ const sendPushNotification = async (somePushTokens, messageBody, data = {}) => {
 
     let chunks = expo.chunkPushNotifications(messages);
     let tickets = [];
-    
+
     for (let chunk of chunks) {
         try {
             let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
